55b55dacaf27acaa578949a73cc9996d607d188c
SongJwans <yhh5916@naver.com>
2024-08-20 22:20:10 +0900

    feat: modal 창 pc, 모바일 구현완료(유효성 검사 제외)

--- common.js
+++ common.js
@@ -15,3 +15,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+const scrollToTopBtn = document.querySelector(".scroll-top-btn");
+const footer = document.querySelector("footer");
+
+window.addEventListener("scroll", function () {
+  // 스크롤 위치에 따라 버튼 보이기/숨기기
+  if (window.scrollY > 0) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+
+  // 버튼이 푸터에 닿기 전에 멈추도록 설정
+  const footerTop = footer.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (footerTop <= windowHeight) {
+    scrollToTopBtn.style.position = "absolute";
+    scrollToTopBtn.style.bottom = windowHeight - footerTop + 19 + "px";
+  } else {
+    scrollToTopBtn.style.position = "fixed";
+    scrollToTopBtn.style.bottom = "19px";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+const openModal = document.getElementById("subscribe");
+const modal = document.getElementById("modalContainer");
+const closeModal = document.getElementById("close");
+
+openModal.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
